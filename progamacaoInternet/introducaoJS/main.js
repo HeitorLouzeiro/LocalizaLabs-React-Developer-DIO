24dfb6d8b56408bb89fd6f4e3cbcc5b2e8d0abf5
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-10-03 22:49:43 -0300

    Add initial HTML and JavaScript files

---
+++ progamacaoInternet/introducaoJS/main.js
@@ -0,0 +1,16 @@
+alert("My alert!"); // My alert!
+
+var nome = "João";
+var idade = 20;
+var peso = 70.5;
+var humano = true;
+alert(nome); // João
+alert(idade); // 20
+alert(peso); // 70.5
+alert(humano); // true
+console.log(nome, idade, peso, humano); // João 20 70.5 true
+
+var frase = "Japão é o melhor time do mundo!";
+console.log(frase.replace("Japão", "Brasil")); // Brasil é o melhor time do mundo!
+console.log(frase.toUpperCase()); // JAPÃO É O MELHOR TIME DO MUNDO!
+console.log(frase.toLowerCase()); // japão é o melhor time do mundo!
